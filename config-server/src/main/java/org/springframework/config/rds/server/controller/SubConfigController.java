ef0cdf13d602ce1a2fb44c13660a3ba64abdf45c
276726581 <276726581@qq.com>
2018-11-14 13:33:46 +0800

    spring.cloud.config.server.prefix

--- config-server/src/main/java/org/springframework/config/rds/server/controller/SubConfigController.java
+++ config-server/src/main/java/org/springframework/config/rds/server/controller/SubConfigController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cloud/config")
+@RequestMapping("${spring.cloud.config.server.prefix}")
 public class SubConfigController {
 
     @Autowired
