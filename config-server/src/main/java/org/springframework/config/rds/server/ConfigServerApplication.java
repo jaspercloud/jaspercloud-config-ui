e8bb4dbe4bd3d52170960f357b802e12517b9ae9
276726581 <276726581@qq.com>
2018-11-14 13:35:08 +0800

    @EnableSwagger2

--- config-server/src/main/java/org/springframework/config/rds/server/ConfigServerApplication.java
+++ config-server/src/main/java/org/springframework/config/rds/server/ConfigServerApplication.java
@@ -3,11 +3,9 @@ package org.springframework.config.rds.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableSwagger2
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
