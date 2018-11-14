f8545b82f917e7ee5ca350ed0837465bb08c993a
276726581 <276726581@qq.com>
2018-11-14 18:09:35 +0800

    修改group_group,content字段

--- config-server/doc/init.sql
+++ config-server/doc/init.sql
@@ -12,7 +12,7 @@
  Target Server Version : 90609
  File Encoding         : 65001
 
- Date: 14/11/2018 13:18:28
+ Date: 14/11/2018 18:09:11
 */
 
 
@@ -28,10 +28,10 @@ START 1
 CACHE 1;
 
 -- ----------------------------
--- Sequence structure for seq_service_config
+-- Sequence structure for seq_config_info
 -- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."seq_service_config";
-CREATE SEQUENCE "public"."seq_service_config" 
+DROP SEQUENCE IF EXISTS "public"."seq_config_info";
+CREATE SEQUENCE "public"."seq_config_info" 
 INCREMENT 1
 MINVALUE  1
 MAXVALUE 9223372036854775807
@@ -45,8 +45,8 @@ DROP TABLE IF EXISTS "public"."config_history";
 CREATE TABLE "public"."config_history" (
   "id" int8 NOT NULL DEFAULT nextval('seq_config_history'::regclass),
   "application" varchar(255) COLLATE "pg_catalog"."default",
-  "namespace" varchar(255) COLLATE "pg_catalog"."default",
-  "config" text COLLATE "pg_catalog"."default",
+  "group_area" varchar(255) COLLATE "pg_catalog"."default",
+  "content" text COLLATE "pg_catalog"."default",
   "ver" int4 DEFAULT 0,
   "create_time" timestamp(6) DEFAULT now()
 )
@@ -57,10 +57,10 @@ CREATE TABLE "public"."config_history" (
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."config_info";
 CREATE TABLE "public"."config_info" (
-  "id" int8 NOT NULL DEFAULT nextval('seq_service_config'::regclass),
+  "id" int8 NOT NULL DEFAULT nextval('seq_config_info'::regclass),
   "application" varchar(255) COLLATE "pg_catalog"."default",
-  "namespace" varchar(255) COLLATE "pg_catalog"."default",
-  "config" text COLLATE "pg_catalog"."default",
+  "group_area" varchar(255) COLLATE "pg_catalog"."default",
+  "content" text COLLATE "pg_catalog"."default",
   "ver" int4 DEFAULT 0
 )
 ;
@@ -68,8 +68,8 @@ CREATE TABLE "public"."config_info" (
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
-SELECT setval('"public"."seq_config_history"', 20, true);
-SELECT setval('"public"."seq_service_config"', 44, true);
+SELECT setval('"public"."seq_config_history"', 45, true);
+SELECT setval('"public"."seq_config_info"', 74, true);
 
 -- ----------------------------
 -- Primary Key structure for table config_history
@@ -79,4 +79,4 @@ ALTER TABLE "public"."config_history" ADD CONSTRAINT "config_history_pkey" PRIMA
 -- ----------------------------
 -- Uniques structure for table config_info
 -- ----------------------------
-ALTER TABLE "public"."config_info" ADD CONSTRAINT "service_config_application_namespace_key" UNIQUE ("application", "namespace");
+ALTER TABLE "public"."config_info" ADD CONSTRAINT "service_config_application_namespace_key" UNIQUE ("application", "group_area");
